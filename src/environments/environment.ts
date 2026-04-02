52f7478e442cc2ad53bd8a922bd9b2286620dea9
datpt1 <datpt1@vnpay.vn>
2026-04-02 17:12:32 +0700

    Public web

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '' // Khi chạy trên Vercel, chúng ta sẽ để trống để dùng Proxy hoặc config cụ thể sau
+  apiUrl: 'https://toolcnc-api.onrender.com' // Khi chạy trên Vercel, chúng ta sẽ để trống để dùng Proxy hoặc config cụ thể sau
 };
